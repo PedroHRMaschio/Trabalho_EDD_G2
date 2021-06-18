0cbff9beaee1e1566d074e15c68ac058bdbb753d
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2021-06-17 22:48:52 -0300

    normalicao e pickle

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,17 @@
-import codecs
+import codecs, pickle
+from unicodedata import normalize
 
-def stopwords_def(nome_arquivo):
+def funcao_2_aux(nome_arquivo):
 
+    #criar um array com as pontuações para remover pontuação depois
+    pontuacao = []
+    arqpontuacao = codecs.open("test/pontuacao.txt", "r", "UTF-8")
+    linhas = arqpontuacao.readlines()
+    for linha in linhas:
+        pontuacao.append(linha.replace('\n', '').strip().lower())
+    arqpontuacao.close()
+
+    #criar um array com stopwords para remover depois
     stopwords = []
     arq = codecs.open("test/stopwords.txt", "r", "UTF-8")
     linhas = arq.readlines()
@@ -9,7 +19,8 @@ def stopwords_def(nome_arquivo):
         stopwords.append(linha.replace('\n', '').strip().lower())
     arq.close()
 
-    arquivo = codecs.open(nome_arquivo, "r", "UTF-8")
+    #realizar tokenização
+    arquivo = codecs.open("docs/"+nome_arquivo+".txt", "r", "UTF-8")
     linhas_arquivo = arquivo.readlines()
     palavras = []
     for linha in linhas_arquivo:
@@ -17,20 +28,42 @@ def stopwords_def(nome_arquivo):
             palavras.append(palavra.replace('\n', '').strip().lower())
     arquivo.close()
 
+    #remover stopwords
     apos_remover_stopwords = []
     for p in palavras:
         if (p.upper() in stopwords) or (p.lower() in stopwords):
             continue
         else:
             apos_remover_stopwords.append(p)
-    
-    return apos_remover_stopwords
+
+    #remover pontuação e acentos
+    apos_remover_pontuacao = []
+    for p in apos_remover_stopwords:
+        if "".__eq__(p):
+            continue
+        else:
+            p = normalize('NFKD', p).encode('ASCII','ignore').decode('ASCII')
+            for letra in p:
+                if (letra in pontuacao):
+                    p = list(p)
+                    p.remove(letra)
+                    p = "".join(p)
+                else:
+                    continue
+            apos_remover_pontuacao.append(p)
+
+    #Realizando o salvamento do objeto com a biblioteca pickle
+    arquivo = open("docs/"+nome_arquivo+"_pickle.pckl", "wb")
+    arquivo.write(pickle.dumps(apos_remover_pontuacao))
+    arquivo.close()
+
 
 def funcao_1():
     documento = input("Digite aqui o nome do seu documento: ")
     arquivo = open("docs/"+documento+".txt", "w")
     escrita = input("\nDigite aqui o que você deseja colocar no seu documento: ")
     arquivo.write(escrita)
+
 
 def funcao_2():
     pass
